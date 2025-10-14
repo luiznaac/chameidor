f0c0b4eef68f45f2be456c12ce717a4a1333933e
agner <luiz@agner.dev>
2025-10-14 20:27:06 -0300

    separating json modules

---
+++ usecase/src/main/kotlin/dev/agner/chameidor/usecase/configuration/serializers/CronExpressionModule.kt
@@ -0,0 +1,31 @@
+package dev.agner.chameidor.usecase.configuration.serializers
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.module.SimpleModule
+import org.springframework.scheduling.support.CronExpression
+
+class CronExpressionModule : SimpleModule() {
+    init {
+        addSerializer(
+            CronExpression::class.java,
+            object : JsonSerializer<CronExpression>() {
+                override fun serialize(value: CronExpression, gen: JsonGenerator, serializers: SerializerProvider) {
+                    gen.writeString(value.toString().replaceFirst("* ", ""))
+                }
+            },
+        )
+        addDeserializer(
+            CronExpression::class.java,
+            object : JsonDeserializer<CronExpression>() {
+                override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+                    CronExpression.parse("* " + p.codec.readTree<JsonNode>(p).textValue())
+            },
+        )
+    }
+}
